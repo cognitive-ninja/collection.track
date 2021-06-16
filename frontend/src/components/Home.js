38c3899a0a341059f90e911a023f9a0c237ebd6d
cognitive-ninja <saurabhhirugade@gmail.com>
2021-06-16 15:14:10 +0530

    Styling updated

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,12 +4,15 @@ import "../App.css"
 
 export const Home = () => {
     return (
-        <div className="home" style={{height:"100vh"}}>
+        <>
+        <div className="home">
             <div className="head-section">
                 <img src={logo} className="logo" alt="logo"></img>
                 <p className="title">collection.track</p>
             </div>
             <p className="slogan">EXPLORE, ORGANIZE AND KEEP TRACK OF YOUR COLLECTION</p>
         </div>
+        <div style={{textAlign:"center",padding:"3px", backgroundColor:"black", color:"white"}}>Developed with <i class="fas fa-heart" style={{fontSize:"medium", marginBottom:"-3px", color:"rgb(215,0,0)"}}></i> by <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/saurabh-hirugade/" style={{textDecoration:"none", color:"lightgrey", fontStyle:"italic"}}>Saurabh Hirugade</a></div>
+        </>
     )
 }
